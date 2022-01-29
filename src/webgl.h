9a20886ee35d310155f09d520cf3758c715b37ed
Noah Cohn <noahc42@gmail.com>
2022-01-29 09:32:11 -0800

    Update webgl.h

--- src/webgl.h
+++ src/webgl.h
@@ -8,6 +8,4 @@ void fill_solid_rectangle(float x0,float y0,float x1,float y1,float r,float g,fl
 void fill_text(float x0,float y0,float r,float g,float b,float a,const char *str,float spacing,int charSize,int shadow);
 void fill_image(float x0,float y0,float scale,float r,float g,float b,float a,const char *url);
 void play_audio(const char *url,int loop);
-#ifdef __cplusplus
 }
-#endif

92c7b76a7e4aff518b2629450a2f3a542407b24e
Noah Cohn <noahc42@gmail.com>
2022-01-29 09:32:37 -0800

    Update webgl.h

--- src/webgl.h
+++ src/webgl.h
@@ -1,5 +1,4 @@
 #pragma once
-extern "C" {
 void init_webgl(int width,int height);
 void set_animation_frame_callback(void(*func)(double t,double dt));
 double rand01(void);
@@ -8,4 +7,4 @@ void fill_solid_rectangle(float x0,float y0,float x1,float y1,float r,float g,fl
 void fill_text(float x0,float y0,float r,float g,float b,float a,const char *str,float spacing,int charSize,int shadow);
 void fill_image(float x0,float y0,float scale,float r,float g,float b,float a,const char *url);
 void play_audio(const char *url,int loop);
-}
+

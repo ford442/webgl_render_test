1ab2737511a3920b51663584ac1b7dd019161ca6
Noah Cohn <noahc42@gmail.com>
2022-01-29 13:59:36 -0800

    Update setUp.js

--- src/setUp.js
+++ src/setUp.js
@@ -1,8 +1,11 @@
 function opn(){
+document.getElementById('pmhig').innerHTML=window.innerHeight;
+setTimeout(function(){
 document.getElementById('shut').innerHTML=2;
 document.getElementById("circle").width=window.innerWidth;
 document.getElementById("circle").height=window.innerHeight;
 let $hg=window.innerHeight+"px";
 document.getElementById("wrap").style.lineheight=$hg;
-document.getElementById('pmhig').innerHTML=window.innerHeight;
+document.getElementById("di").click();
+},500);};
 };

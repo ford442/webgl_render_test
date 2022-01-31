1c4f521d1c667f3837fa65af1cc2681bb68b2d93
Noah Cohn <noahc42@gmail.com>
2022-01-30 16:52:15 -0800

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -71,10 +71,11 @@ pixelWidth=2.0f/width;
 pixelHeight=2.0f/height;
 static const char vertex_shader[]=
 
-"#version 300 es\n"
-"layout(location=0)in vec4 pos;out vec2 uv;uniform mat4 mat;"
+"#version 300 es \n"
+"precision mediump float;in vec4 pos;out vec2 uv;uniform mat4 mat;"
 "void main(){uv=pos.xy;gl_Position=mat*pos;}"
-"\n\0";  /*
+"\n\0";  
+  /*
 "#version 100 \n"
 "attribute vec4 pos;"
 "varying vec2 uv;"
@@ -85,7 +86,7 @@ static const char vertex_shader[]=
 "} \n\0"; */
 GLuint vs=compile_shader(GL_VERTEX_SHADER,vertex_shader);
 static const char fragment_shader[]=
-"#version 300 es\n"
+"#version 300 es \n"
 "precision mediump float;out vec4 gl_FragColor;"
 "uniform sampler2D tex;in vec2 uv;uniform vec4 color;"
 "void main(){gl_FragColor=color*texture(tex,uv);}"

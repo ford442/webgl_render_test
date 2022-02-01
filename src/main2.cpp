f9cbc71dcf7426f9fe582417ceaf53678d834512
Noah Cohn <noahc42@gmail.com>
2022-01-31 16:55:28 -0800

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -178,9 +178,8 @@ fill_solid_rectangle(FX+x*BX,FY+y*BY+wy,FX+(x+1)*BX,FY+(y+1)*BY+wy,c?0.f:1.f,c?4
 fill_image(250.0f,10.0f,1.0f,1.0f,1.0f,1.0f,1.0f,"reindeer.png");
 }
 int main(){
-S=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
-S=800;
-init_webgl(S,S);
+emscripten_get_element_css_size("#canvas",S,S);
+  init_webgl(S,S);
 printf("%u \n",S);
 emscripten_set_main_loop((void(*)())draw_frame,0,0);
 }
